d926a8813b732b37c7ff77c5608bd9ea9a38e63a
Steve Rayner <srayner02@gmail.com>
2016-03-07 21:00:33 +0000

    Added controller and view

---
+++ src/CivUser/Controller/UserController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace CivUser\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+
+class UserController extends AbstractActionController
+{
+    public function loginAction()
+    {
+        return array();
+    }
+}
+
